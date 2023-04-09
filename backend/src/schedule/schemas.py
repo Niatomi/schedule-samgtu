c573fa3960380e8025ff95a35fcfa13905c81ac1
Niatomi <playervoker@gmail.com>
2023-04-09 18:39:09 +0400

    Fully deleted

--- backend/src/schedule/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-from datetime import timezone
-
-class ScheduleSchema(BaseModel):
-    name: str
-    datetime_start: datetime
-    datetime_end: datetime
