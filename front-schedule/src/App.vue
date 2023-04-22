16ecc9196d4d65ce396b7d5664f05387d9d1d368
Niatomi <playervoker@gmail.com>
2023-04-22 23:00:57 +0400

    Full recommit

--- front-schedule/src/App.vue
+++ front-schedule/src/App.vue
@@ -1,3 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
 /* Указываем box sizing */
 *,
 *::before,
@@ -76,3 +92,4 @@ select {
     scroll-behavior: auto !important;
   }
 }
+</style>
